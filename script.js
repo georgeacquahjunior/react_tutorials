08e538bd42d0a008de72fc4af5b1e5dc01c7221f
georgeacquahjunior <georgeacquahjunior@gmail.com>
2025-09-23 07:05:22 +0000

    Add script.js with normal and arrow function examples

---
+++ script.js
@@ -0,0 +1,12 @@
+// normal function representation
+hello = function(val){
+    return "Hello World! " + val;
+}
+
+hello2 = (val) => {
+    return "Hello World!! "+ val;
+}
+
+console.log(hello(1));
+
+
